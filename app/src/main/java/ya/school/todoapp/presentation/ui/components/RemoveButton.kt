2360c30e584d933fe7daa3c4e0e1ca22658f23f4
mrGnost <mrgnost@yandex.ru>
2024-07-20 04:08:28 +0300

    Added material color scheme

--- app/src/main/java/ya/school/todoapp/presentation/ui/components/RemoveButton.kt
+++ app/src/main/java/ya/school/todoapp/presentation/ui/components/RemoveButton.kt
@@ -1,13 +1,17 @@
 package ya.school.todoapp.presentation.ui.components
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ya.school.todoapp.presentation.ui.theme.AppTheme
@@ -20,7 +24,12 @@ fun RemoveButton(
 ) {
     Row(
         modifier = modifier
-            .clickable {
+            .clickable(
+                interactionSource = remember {
+                    MutableInteractionSource()
+                },
+                indication = rememberRipple()
+            ) {
                 if (isActive)
                     onClick()
             }
